d6f35e34705d8aeb0a5b9a880361d203af7071b2
gfrancoa <gfrancoa@outlook.com>
2023-07-06 13:32:04 -0400

    updating servlet path products

--- store/src/main/java/basharatkaranwalfranco/store/controllers/ProductController.java
+++ store/src/main/java/basharatkaranwalfranco/store/controllers/ProductController.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-@WebServlet(name = "ProductController", urlPatterns = {"/products", ""})
+@WebServlet(name = "ProductController", urlPatterns = {"/products"})
 public class ProductController extends HttpServlet {
 
     @Override
